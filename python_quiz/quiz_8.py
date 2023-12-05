73a7103856ea852274f898661573c7389f4bd54a
mahmoudessam820 <esame4166@gmail.com>
2023-12-06 01:50:31 +0200

    added new quiz 8

---
+++ python_quiz/quiz_8.py
@@ -0,0 +1,17 @@
+# Python Quiz (8)😉
+
+# What is the output of this code and why 🤔?
+
+def func():
+
+    lst, lst2 = [7], [7]
+    lst3 = lst * 2
+
+    return lst3 == lst2.extend(lst2)
+
+# Output
+
+# a. True
+# b. False
+# c. Error
+# d. [7, 7]
