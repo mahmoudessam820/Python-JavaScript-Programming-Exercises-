e7d5aee4ed72c838dab3f5a2b5663505d6d25325
mahmoudessam820 <esame4166@gmail.com>
2024-01-31 02:02:04 +0200

    update the explanation of python quiz 16

--- python_quiz/quiz_16.py
+++ python_quiz/quiz_16.py
@@ -18,12 +18,12 @@ print(d)
 
 # Explanation:
 
-# The code starts by creating two sets, a and b.
-# Set a contains the elements 1, 2, 3, and 4, while set b contains the elements 3, 4, and 5.
-# The next line c, *d = a & b performs the intersection operation between sets a and b. 
-# The intersection operation (&) returns a new set that contains the common elements present in both sets.
-# The interesting part is the syntax c, *d. Here, c is a single variable assigned to the first element of the intersection set (in this case, 3).
-# The asterisk (*) before d is used to assign the remaining elements of the intersection set to d.
-# Finally, the code prints the value of d, which will be [4]. 
-# This is because d holds the remaining elements of the intersection set other than the first element (3).
 
+# a = {1, 2, 3, 4}: This line declares a set named a and initializes it with the elements 1, 2, 3, and 4.
+# b = {3, 4, 5}: This line declares a set named b and initializes it with the elements 3, 4, and 5.
+# c, *d = a & b: This line performs a set intersection operation between sets a and b using the & operator. 
+# The intersection of two sets returns a new set with elements that are common in both sets. 
+# The result is assigned to two variables: c and d. 
+# The variable c will store an element common to both sets (c = 3 in this case), and d will store the remaining elements from the intersection (d = [4] in this case). 
+# The use of the * operator before d is called the "extended iterable unpacking" syntax and allows d to capture all remaining elements from the intersection.
+# Finally, the code prints the value of d, which will be [4]. 
